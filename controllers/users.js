91860fab73217ed53cfdc2393fb4fcaf3bc6ed46
Svetlana Andreeva <5231328@gmail.com>
2023-04-11 22:14:23 +0300

    fix: error

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,9 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => {
-      res.send(user);
+      if (user) {
+        res.send(user);
+      }
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
