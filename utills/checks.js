5db6c6fb654f7aa6280dc97b37262f63c6c644f2
Svetlana Andreeva <5231328@gmail.com>
2023-04-12 12:52:21 +0300

    fix: error

--- utills/checks.js
+++ utills/checks.js
@@ -1,21 +1,25 @@
+const BAD_REQUEST_ERROR = 400;
+const NOT_FOUND_ERROR = 404;
+const SERVER_ERROR = 500;
+
 const checkId = (id, res) => {
   if (!id) {
-    return res.status(404).send({ message: 'данные не найдены' });
+    return res.status(NOT_FOUND_ERROR).send({ message: 'данные не найдены' });
   }
   return res.send(id);
 };
 
 const checkErrors = (err, res) => {
   if (err.name === 'ValidationError') {
-    return res.status(400).send({ message: 'переданы некорректные данные' });
+    return res.status(BAD_REQUEST_ERROR).send({ message: 'переданы некорректные данные' });
   }
   if (err.name === 'CastError') {
-    return res.status(400).send({ message: 'переданы некорректные данные' });
+    return res.status(BAD_REQUEST_ERROR).send({ message: 'переданы некорректные данные' });
   }
   if (err.name === 'DocumentNotFoundError') {
-    return res.status(404).send({ message: 'данные не найдены' });
+    return res.status(NOT_FOUND_ERROR).send({ message: 'данные не найдены' });
   }
-  return res.status(500).send({ message: 'ошибка сервера' });
+  return res.status(SERVER_ERROR).send({ message: 'ошибка сервера' });
 };
 
 module.exports = {
